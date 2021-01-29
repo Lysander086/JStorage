da0ac172b3bb022c98448ce8789d3109ecae815d
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-01-29 23:39:36 +0800

    hi world

--- src/main/java/com/example/JStorageApp.java
+++ src/main/java/com/example/JStorageApp.java
@@ -3,11 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class KeyValueStorageApplication {
-
+public class JStorageApp {
     public static void main(String[] args) {
-        SpringApplication.run(KeyValueStorageApplication.class, args);
+        SpringApplication.run(JStorageApp.class, args);
     }
-
 }
--- src/test/java/com/example/JStorageApplicationTests.java
+++ src/test/java/com/example/JStorageApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KeyValueStorageApplicationTests {
+class JStorageApplicationTests {
 
     @Test
     void contextLoads() {
