8fac9e11594f32d23dd78da84f34420626b40966
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-03-03 22:29:48 +0800

    accessible

--- src/main/java/com/example/a_experiments/PlayJava.java
+++ src/main/java/com/example/a_experiments/PlayJava.java
@@ -1,18 +1,72 @@
 package com.example.a_experiments;
 
 import com.example.entity.Pair;
+import lombok.SneakyThrows;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class PlayJava {
 
+    @SneakyThrows
     public static void main(String[] args) {
         TreeMap<Long, Pair> tm = new TreeMap();
         long kTime = System.currentTimeMillis();
+
+        long leftK = 1614781620860L;
+
+        tm.put(leftK, new Pair("e", "eye"));
+
+        setAccessible(TreeMap.class);
         tm.put(kTime, new Pair("f", "fly"));
 
-//        Pair res = tm.getEntry(kTime);
+        Method getEntry = TreeMap.class.getDeclaredMethod("getEntry", Object.class);
+
+        getEntry.setAccessible(true);
+
+        Map.Entry res = (Map.Entry) getEntry.invoke(tm, kTime);
+
+//
+//        System.out.println(res.getClass().toString());
+//
+//        System.out.println(res.getKey().toString());
+//
+//        System.out.println(res.getValue().getClass());
+
+//        System.out.println(res.left);
+
+    }
+
+
+    public static void setAccessible(Class clazz) {
+        setAccessible(clazz, true, true);
+    }
+
+    public static void setAccessible(Class clazz, boolean toSetMethods, boolean toSetFields) {
+
+        if (toSetMethods) {
+            Method[] methods = clazz.getDeclaredMethods();
+
+            for (Method m : methods) {
+                m.setAccessible(true);
+            }
+        }
+
+        if (toSetFields) {
+            Field[] fields = TreeMap.class.getDeclaredFields();
+
+            for (Field field : fields) {
+                field.setAccessible(true);
+
+            }
+
+
+        }
 
 
     }
+
+
 }
